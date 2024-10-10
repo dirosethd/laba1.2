b3395bf8b436974086c7f3695f905ea696269463
dirose <dirose@DESKTOP-PQA70RD>
2024-10-10 14:07:28 +0200

    Добавьте файлы проекта.

---
+++ laba1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    const double q = 2;
+    const double b = 1.8;
+
+    double t = Math.Pow(b, 3) + Math.Exp(Math.Sqrt(q));
+    double x = Math.Pow(t, 3) + b * b;
+    double y = Math.Pow(Math.Atan(Math.Abs(x)), 2);
+    Console.WriteLine($"t={t:F2}, x={x:F2}, y={y:F2}");
+}
+catch
+{
+    Console.WriteLine("Введите правильно");
+}
